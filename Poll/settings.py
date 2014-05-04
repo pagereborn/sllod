7935e27a620d92c52bc06135d7c4460f1f28e988
Steven <steven@steven-VirtualBox.(none)>
2014-05-03 23:34:20 -0400

    setting changes

--- Poll/settings.py
+++ Poll/settings.py
@@ -120,8 +120,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
